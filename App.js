db75643d6581e22cde5bb721853d09ebcb80c5c0
Ramkumarkarne <ramkumar73559@gmail.com>
2025-04-02 14:18:49 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Anurag from './canurag';
+
+function App() {
+  return (
+    <div>
+    <div><h1 className='anurag'>Hey Anurag</h1></div>
+    <div className="App">
+
+     <Anurag name="Anurag" city="kodad"/>
+     <Anurag name="CBIT" city="khammam"/>
+     <Anurag name="SBIT" city="khammam"/>
+     <Anurag name="KITS" city="guntur"/>
+      <h1>Auditorium C1</h1>
+    </div>
+    </div>
+   
+  );
+}
+
+export default App;
